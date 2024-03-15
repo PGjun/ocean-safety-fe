49e936d35636ec1d43e25482efa6d310d574857e
Gjun <pgj127@gmail.com>
2024-03-15 12:54:43 +0900

    QRcodeScanner 추가

---
+++ src/app/qr/page.tsx
@@ -0,0 +1,20 @@
+// pages/index.js 또는 다른 페이지 컴포넌트
+'use client'
+
+import dynamic from 'next/dynamic'
+import React from 'react'
+
+const QRCodeScanner = dynamic(() => import('@/app/qr/QRCodeScanner '), {
+  ssr: false, // 서버 사이드 렌더링을 비활성화
+})
+
+const Home = () => {
+  return (
+    <div>
+      <h1>QR 코드 스캐너</h1>
+      <QRCodeScanner />
+    </div>
+  )
+}
+
+export default Home
