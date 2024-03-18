29da6f72e23a371caa2861e80d7c937993e7adf1
gjun <pgj127@gmail.com>
2024-03-18 18:38:54 +0900

    마진 패딩으로 변경 모바일

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import { CrewStatus } from '@/components/main/CrewStatus'
 export default function Home() {
   return (
     <main className="m-auto mt-[32px] flex justify-center">
-      <div className="m-[16px] grid h-screen gap-[32px] md:w-[1360px] md:grid-cols-2 ">
+      <div className="grid h-screen gap-[32px] px-[16px] md:w-[1360px] md:grid-cols-2 ">
         <CrewStatus />
         <CrewHealthInfo />
         <CrewSos />
