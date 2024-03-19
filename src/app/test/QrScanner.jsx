1c6a1dd7cb1e9717e34566eaf81d36cde77fcc77
Gjun <pgj127@gmail.com>
2024-03-19 15:44:53 +0900

    큐알 테스트 완료

--- src/app/test/QrScanner.jsx
+++ src/app/test/QrScanner.jsx
@@ -6,6 +6,7 @@ import dynamic from 'next/dynamic'
 const QrReader = dynamic(() => import('react-qr-scanner'), { ssr: false })
 
 const QrScanner = () => {
+  const [qrOpen, setQrOpen] = useState(false)
   const [result, setResult] = useState('No result')
 
   const handleScan = (data) => {
@@ -23,15 +24,45 @@ const QrScanner = () => {
     width: 320,
   }
 
+  // useEffect(() => {
+  //   if (result !== 'No result') {
+  //     window.open(result)
+  //   }
+  // }, [result])
+
   return (
     <div>
-      <QrReader
-        delay={100}
-        style={previewStyle}
-        onError={handleError}
-        onScan={handleScan}
-      />
-      <p>{result}</p>
+      <button
+        onClick={() => {
+          setResult('No result')
+          setQrOpen(!qrOpen)
+        }}
+        className="rounded-xl bg-blue-500 p-[10px] text-[24px] font-bold text-white"
+      >
+        QR 스캔
+      </button>
+      {qrOpen && (
+        <>
+          <QrReader
+            delay={100}
+            style={previewStyle}
+            onError={handleError}
+            onScan={handleScan}
+          />
+          <p>
+            {result !== 'No result' && (
+              <button
+                onClick={() => {
+                  window.open(result)
+                }}
+                className="rounded-xl border border-blue-500 p-[10px] text-[24px] font-bold "
+              >
+                {result}
+              </button>
+            )}
+          </p>
+        </>
+      )}
     </div>
   )
 }
