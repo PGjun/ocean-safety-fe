088a2d428ce78901ec2a11f5d03780c851546efb
PGjun <pgj127@gmail.com>
2024-04-02 14:08:39 +0900

    콤보박스 작업중

--- src/app/(page)/crewinfo/components/AreaSettings.tsx
+++ src/app/(page)/crewinfo/components/AreaSettings.tsx
@@ -1,16 +1,63 @@
 import Image from 'next/image'
+import { useState } from 'react'
+
+const dropData = [
+  [
+    { id: 0, name: '강원호' },
+    { id: 1, name: '무궁화호' },
+  ],
+  [
+    { id: 0, name: '강원호2' },
+    { id: 1, name: '무궁화호2' },
+  ],
+  [
+    { id: 0, name: '강원호3' },
+    { id: 1, name: '무궁화호3' },
+  ],
+]
 
 export const AreaSettings = () => {
+  const [selectedGroupIndex, setSelectedGroupIndex] = useState(0)
+
+  const handlePrev = () => {
+    setSelectedGroupIndex((prevIndex) =>
+      prevIndex > 0 ? prevIndex - 1 : dropData.length - 1,
+    )
+  }
+
+  const handleNext = () => {
+    setSelectedGroupIndex((prevIndex) =>
+      prevIndex < dropData.length - 1 ? prevIndex + 1 : 0,
+    )
+  }
+
+  const handleChange = (event: any) => {
+    console.log(event.target.value) // 선택된 선박의 이름을 처리
+  }
+
   return (
     <div className="mt-[10px] p-[10px]">
       <div className="flex flex-col items-start justify-end gap-2 md:flex-row md:items-center">
         <span className="text-[14px] md:text-[16px]">선박 선택</span>
         <div className="flex rounded border border-[#C4C4C4]">
-          <button className="border-r border-[#C4C4C4] px-[13px]">{`<`}</button>
-          <div className="px-[90px] py-[10px] text-[12px] md:text-[14px]">
-            강원호
-          </div>
-          <button className="border-l border-[#C4C4C4] px-[13px]">{`>`}</button>
+          <button
+            className="border-r border-[#C4C4C4] px-[13px]"
+            onClick={handlePrev}
+          >{`<`}</button>
+          <select
+            onChange={handleChange}
+            className="px-[90px] py-[10px] text-[12px] md:text-[14px]"
+          >
+            {dropData[selectedGroupIndex].map((ship) => (
+              <option key={ship.id} value={ship.name}>
+                {ship.name}
+              </option>
+            ))}
+          </select>
+          <button
+            className="border-l border-[#C4C4C4] px-[13px]"
+            onClick={handleNext}
+          >{`>`}</button>
         </div>
       </div>
       <div className="mt-[10px]">
